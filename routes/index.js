46b8f87ae0abad007273ed552d03e217e4fbca2c
Andrew Lorente <hello@andrewlorente.com>
2015-05-26 16:12:23 -0700

    Switch from raw jade to markdown

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var marked = require('marked');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-fs.readdir(__dirname + '/../posts', function(error, directoryContents) {
+var postsDir = __dirname + '/../posts/';
+fs.readdir(postsDir, function(error, directoryContents) {
   if (error) {
     throw new Error(error);
   }
 
   directoryContents.forEach(function(postFileName) {
-    var postName = postFileName.replace('.jade', '');
-    router.get('/' + postName, function(request, response) {
-      response.render('../posts/' + postFileName, {});
-    });
+    var postName = postFileName.replace('.md', '');
+    fs.readFile(postsDir + postFileName, {encoding: 'utf-8'}, function(error, fileContents) {
+      if (error) {
+        throw new Error(error);
+      }
+
+      var renderedPost = marked(fileContents);
+
+      router.get('/' + postName, function(request, response) {
+        response.render('post', {postContents: renderedPost});
+      });
+    })
   });
 });
 
