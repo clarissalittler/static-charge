82e496811e463aa61786b3bbab4dead248beaa6c
Clarissa Littler <clarissa.littler@gmail.com>
2015-05-27 10:57:32 -0700

    First stab at a hierarchy of files included as links beneath the content

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,18 @@ var fs = require('fs');
 var marked = require('marked');
 
 var postsDir = __dirname + '/../posts/';
+var configfile = __dirname + '/../config.json'
 fs.readdir(postsDir, function(error, directoryContents) {
   if (error) {
     throw new Error(error);
   }
 
   var posts = directoryContents.map(function(filename) {
-    var postName = filename.replace('.md', '');
-    var contents = fs.readFileSync(postsDir + filename, {encoding: 'utf-8'});
-    return {postName: postName, contents: marked(contents)};
+      var postName = filename.replace('.md', '');
+      var contents = fs.readFileSync(postsDir + filename, {encoding: 'utf-8'});
+      var hierarchy = JSON.parse(fs.readFileSync(configfile,'utf-8'));
+      console.log(hierarchy);
+      return {postName: postName, contents: marked(contents), children : hierarchy[postName]};
   });
 
   router.get('/', function(request, response) {
@@ -21,7 +24,8 @@ fs.readdir(postsDir, function(error, directoryContents) {
 
   posts.forEach(function(post) {
     router.get('/' + post.postName, function(request, response) {
-      response.render('post', {postContents: post.contents});
+	console.log(post.children);
+      response.render('post', {postContents: post.contents, postChildren: post.children});
     });
   });
 });
